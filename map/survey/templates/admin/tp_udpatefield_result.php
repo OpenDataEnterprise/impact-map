edf135308ea233265116aa83371e9ba243b65296
Noah <hanlun.song@gmail.com>
2016-09-26 19:33:13 -0400

    delete unecessary files / update from webflow / remove map folder for now

--- map/survey/templates/admin/tp_udpatefield_result.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "Field '${content['field_name']}' successfully updated to '".$content['value']."' for profile '${content['profile_id']}'";
-
-?>
--- screenShotReadyVizs/js-custom/db_config_local.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define("DBHOST", "localhost:8889");
-	define("DBNAME", "opendata_db");
-	define("DBUSER", "root");
-	define("DBPASS", "19880518");
-?>
