9bb4817925f7567d147276995f33b96d606435dd
Myeong Lee <deeper.myeong.lee@gmail.com>
2017-05-28 22:37:59 -0400

    put validation in the register page

--- html/admin/register.php
+++ html/admin/register.php
@@ -2,12 +2,12 @@
 include "config.php";
 
 session_start();
-// if (!isset($_SESSION['login_true'])) 
-//     {
-//      echo '<p class="login">Please <a href="login.php">log in</a> to access this page.</p>';
-//     exit();
-// //    header('location: home.php');
-//     }
+if (!isset($_SESSION['login_true'])) 
+    {
+     echo '<p class="login">Please <a href="login.php">log in</a> to access this page.</p>';
+    exit();
+//    header('location: home.php');
+    }
 
 ?>
 <!DOCTYPE html>
