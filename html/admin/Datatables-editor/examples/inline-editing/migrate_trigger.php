5fd8bed94bbdfb195638d995e627ba572260e270
Ubuntu <ubuntu@ip-172-31-52-46.ec2.internal>
2017-12-26 06:56:34 +0000

    migrate trigger updated

--- html/admin/Datatables-editor/examples/inline-editing/migrate_trigger.php
+++ html/admin/Datatables-editor/examples/inline-editing/migrate_trigger.php
@@ -32,8 +32,33 @@ $env = getenv('AGOL_ENV');
 //$output = shell_exec("/var/scripts/agol-integration/agol_integration.py");
 echo "Migrating... <br><br>";
 // $output = exec("agol-integration/agol_integration.py");
-passthru("agol-integration/agol_integration.py", $output_text);
-echo $output_text . "<br>";
+//passthru("agol-integration/agol_integration.py", $output_text);
+
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("file", "/var/log/odesurvey/error-migration.txt", "a") // stderr is a file to write to
+);
+
+$proc = proc_open("python agol-integration/agol_integration.py", $descriptorspec, $pipes);
+
+if (is_resource($proc)) {
+   fclose($pipes[0]);
+
+   $pos = FALSE;   
+   while ($pos === FALSE) {
+	
+        $out = fgets($pipes[1], 300);
+	echo $out . "<br>\n";
+	$pos = strpos($out, "Number of features written to AGOL");
+	sleep(1);
+   }
+   fclose($pipes[1]);
+
+   $return_value = proc_close($proc);
+}
+//echo $output_text . "<br>";
+
 // echo $output;
 echo "<br><br>Done..";
 
