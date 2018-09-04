40bcfccca37550141cb46b58fa51a410eaca649e
Andy Lo <squeakyspacebar@gmail.com>
2018-09-04 15:39:47 -0500

    Added split vendor bundles.

--- html/scripts/vis/renderBarCharts.js
+++ html/scripts/vis/renderBarCharts.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import d3Tip from 'd3-tip';
+import $ from 'jquery';
 
 function wrap (text, width) {
   text.each(function () {
--- html/webpack.config.js
+++ html/webpack.config.js
@@ -6,18 +6,17 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const entryPlus = require('webpack-entry-plus');
 
 function defaultFilename (filename) {
-  return filename.split('/').pop();
+  return filename.split('/').pop().replace(/\.js$/, '');
 }
 
 // Define entry points here (to be processed through webpack-entry-plus).
 const entryFiles = [
   {
-    entryFiles: './scripts/index.js',
-    outputName: 'index.js',
-  },
-  {
-    entryFiles: './scripts/useCases.js',
-    outputName: 'useCases.js',
+    entryFiles: [
+      './scripts/index.js',
+      './scripts/useCases.js',
+    ],
+    outputName: defaultFilename,
   },
   {
     entryFiles: glob.sync('./scripts/regions/*.js'),
@@ -33,19 +32,30 @@ module.exports = () => {
   return {
     entry: entryPlus(entryFiles),
     output: {
-      filename: '[name]',
-      path: path.resolve(__dirname, './dist/js')
+      filename: '[name].js',
+      path: path.resolve(__dirname, './dist/js'),
     },
     module: {
       rules: [
         {
           test: /\.js$/,
           use: {
-            loader: 'babel-loader'
+            loader: 'babel-loader',
           },
-          exclude: /(node_modules)/
-        }
-      ]
+          exclude: /(node_modules)/,
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'initial',
+          },
+        },
+      }
     },
     plugins: [
       new BundleAnalyzerPlugin({ analyzerMode: 'disable' }),
@@ -54,7 +64,7 @@ module.exports = () => {
     resolve: {
       modules: [
         path.resolve('./scripts'),
-        path.resolve('./node_modules')
+        path.resolve('./node_modules'),
       ]
     }
   };
